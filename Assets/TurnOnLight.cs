24659149f3f07abef210d6e751fa9a2b48700e9e
Coby <4885716+cobyw@users.noreply.github.com>
2020-10-10 14:53:40 -0500

    NPCs now turn on lights
    NPCs now turn on lights
    Also increased the default distance the trickr treaters notice off lights to 10

---
+++ Assets/TurnOnLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnLight : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("TrickRTreater"))
+        {
+            DecoLight dL = transform.parent.gameObject.GetComponent<DecoLight>();
+            dL.TurnOnLight();
+        }
+    }
+
+}
