96b575ff3a82b7bbf45614445d5a1d89c9637de3
Coby <4885716+cobyw@users.noreply.github.com>
2020-10-10 15:24:41 -0500

    Jack can now turn off lights with E
    Take THAT, lights!

---
+++ Assets/Scripts/TurnOffLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffLight : MonoBehaviour
+{
+    private bool isJackInLight = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isJackInLight = true;
+        }
+
+        //something here could be what damages poor little candle
+        if (collision.CompareTag("Candle"))
+        {
+            //nothing yet
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isJackInLight = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isJackInLight && Input.GetKeyDown(KeyCode.E))
+        {
+            DecoLight dL = transform.parent.gameObject.GetComponent<DecoLight>();
+            dL.TurnOffLight();
+        }
+    }
+    
+}
