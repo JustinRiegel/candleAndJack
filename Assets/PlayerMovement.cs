5e0e96632b85bb0d27ff15afe62ba9aa72c9c1a3
Josh <joshuapotts80@gmail.com>
2020-10-10 01:53:16 -0500

    Added ability to move Jack
    Works with WASD, Arrows and joystick

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    // bad place for physics
+    void Update()
+    {
+        // handle input here
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        // default is 50 times per sec 
+        // handle physics here 
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
