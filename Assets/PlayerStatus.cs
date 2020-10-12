05f059c06f60f6524d3aeeccf224a73dc0731676
Josh <joshuapotts80@gmail.com>
2020-10-12 04:17:23 -0500

    Added health bar for jack
    Currently health bar is in a static place on the screen, could be adjusted to attach it to character sprite if we want.

---
+++ Assets/PlayerStatus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update() // This is just to test health bar by causing Space key to deal 1 damage
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
-
     Vector2 movement;
 
     // Update is called once per frame
@@ -19,8 +17,10 @@ public class PlayerMovement : MonoBehaviour
         // handle input here
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
     }
 
+ 
 
     void FixedUpdate()
     {
