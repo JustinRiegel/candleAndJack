43bf8132fe1a1347f8bccca9aa7082fc8cba8927
F1ghtMilk <72535960+F1ghtMilk@users.noreply.github.com>
2020-10-11 19:50:31 -0500

    PlayerPrefab, Cam control Arod playground

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform camTarget;
+    public Vector3 camOffset;
+
+    private void LateUpdate()
+    {
+        //transform.position = new Vector3(camTarget.position.x, camTarget.position.y, camTarget.position.z) + camOffset;
+        transform.position = camTarget.position + camOffset;
+    }
+}
