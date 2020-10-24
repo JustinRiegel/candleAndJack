fdc9ca90e18ec3e47b9eb2489a172793cdb62c31
Coby <4885716+cobyw@users.noreply.github.com>
2020-10-24 14:14:56 -0500

    Candle now teleports to the shadow realm when you reach the end of the tutorial
    be gone, little brother.

---
+++ Assets/TutorialLoss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialLoss : MonoBehaviour
+{
+    [SerializeField] bool forceTutorialLoss = true;
+    [SerializeField] Vector3 theShadowRealm;
+
+    private void Start()
+    {
+        if (theShadowRealm == null)
+        {
+            theShadowRealm = new Vector3(-300f, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (forceTutorialLoss)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                GameObject.FindWithTag("Candle").transform.position = theShadowRealm;
+            }
+        }
+    }
+}
