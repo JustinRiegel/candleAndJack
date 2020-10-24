5cd371853d83b61e94ca8545380a0cf4cd3f9021
Ciarenni <justin.b.riegel@gmail.com>
2020-10-24 12:54:17 -0500

    Changed the LightManager to build a graph of attractors and pathfinders and find the shortest pairing of those, to make the NPC behaviour more natural/intuitive.

--- Assets/Scripts/LightManager.cs
+++ Assets/Scripts/LightManager.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LightManager : MonoBehaviour
 {
     private List <GameObject> attractorList = new List<GameObject>();
     private PathfinderAI[] pathfinderAIs;
+    private List<GraphLine> _graphLineList = new List<GraphLine>();
+    private GraphLineComparer _graphLineComparer = new GraphLineComparer();
 
     void Start()
     {
@@ -22,41 +25,81 @@ public class LightManager : MonoBehaviour
         attractorList.Remove(removedAttractor);
         updatePathfinderTargets();
     }
-    
-    public GameObject FindClosestAttractor(GameObject source)
-    {
-        Vector2 sourceLocation = source.transform.position;
-        float lowestDistance = float.MaxValue;
-        GameObject retval = null;
-
-        foreach(GameObject attractor in attractorList)
-        {
-            float currentDistance = Vector2.Distance(sourceLocation, attractor.transform.position);
-            if (currentDistance < lowestDistance)
-            {
-                lowestDistance = currentDistance;
-                retval = attractor;
-            }
-        }
-        return retval;
-    }
 
     private void updatePathfinderTargets()
     {
+        //clear out the graph
+        _graphLineList.Clear();
+
+        //clear out targets of pathfinders
         foreach(var npc in pathfinderAIs)
         {
             npc.SetNewTarget(null);
         }
-
-        foreach (PathfinderAI pathfinder in pathfinderAIs)
+        
+        foreach(var light in attractorList)
         {
-            if (pathfinder.GetCanBeDistractedByAttractor())//i dont think this check is needed anymore since candle is no longer a pathfinder
-            {
-                var closestAttractor = FindClosestAttractor(pathfinder.gameObject);
-                closestAttractor.GetComponentInParent<DecoLight>().SetCurrentSeeker(pathfinder);
+            //clear out seekers of attractors
+            light.GetComponentInParent<DecoLight>().SetCurrentSeeker(null);
 
-                pathfinder.SetNewTarget(closestAttractor);
+            foreach(var npc in pathfinderAIs)
+            {
+                //if the npc can be distracted by a light being off, add a line to the graph between the current light and this npc
+                if (npc.GetCanBeDistractedByAttractor())
+                {
+                    _graphLineList.Add(new GraphLine(light, npc));
+                }
             }
+        }
+
+        //sort the list by distance
+        _graphLineList.Sort(_graphLineComparer);
+
+        foreach(var graphLine in _graphLineList)
+        {
+            //if the light does not have a seeker and if the pathfinder does not have a target, this is the closest pathfinder for that light
+            if(graphLine.DecoLightOfNode.GetCurrentSeeker() == null && graphLine.PathfinderNode.GetTarget() == null)
+            {
+                graphLine.DecoLightOfNode.SetCurrentSeeker(graphLine.PathfinderNode);
+                graphLine.PathfinderNode.SetNewTarget(graphLine.AttractorNode);
+            }
+        }
+    }
+
+    private class GraphLine
+    {
+        public GameObject AttractorNode { get; private set; }
+        public DecoLight DecoLightOfNode { get; private set; }
+        public PathfinderAI PathfinderNode { get; private set; }
+        public float Distance { get; private set; }
+
+        public GraphLine(GameObject light, PathfinderAI path)
+        {
+            AttractorNode = light;
+            DecoLightOfNode = AttractorNode.GetComponentInParent<DecoLight>();
+            PathfinderNode = path;
+
+            Vector2 lightNodeLocation = AttractorNode.transform.position;
+            Vector2 pathfinderLocation = PathfinderNode.transform.position;
+
+            Distance = Vector2.Distance(lightNodeLocation, pathfinderLocation);
+        }
+    }
+
+    private class GraphLineComparer : IComparer<GraphLine>
+    {
+        public int Compare(GraphLine x, GraphLine y)
+        {
+            if(x.Distance < y.Distance)
+            {
+                return -1;
+            }
+            else if (x.Distance > y.Distance)
+            {
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
