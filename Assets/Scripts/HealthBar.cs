00de19f617eca76a6ebbad32e86c9c5bec3efcff
Coby <4885716+cobyw@users.noreply.github.com>
2020-10-24 17:26:36 -0500

    Added scary screen effect when the player is far from jack
    GameCanvas prefab has been updated to support this!

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,10 +6,18 @@ public class HealthBar : MonoBehaviour
 {
 
     private Slider slider;
+    public CanvasGroup screenEffect;
+    [Range(1f, 100f)]
+    public float maxScreenEffectHealthPercent = 70f;
 
     private void Awake()
     {
         slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        maxScreenEffectHealthPercent /= 100;
     }
 
     public void SetMaxHealth(int health)
@@ -21,5 +29,18 @@ public class HealthBar : MonoBehaviour
     public void SetHealth(int health)
     {
         slider.value = health;
+        if (screenEffect != null)
+        {
+            float currentHealthPercent = (float)health / (float)slider.maxValue;
+            if (currentHealthPercent <= maxScreenEffectHealthPercent)
+            {
+                var currentLerpVal = currentHealthPercent / maxScreenEffectHealthPercent;
+                screenEffect.alpha = Mathf.Lerp(1f, 0f, currentLerpVal);
+            }
+        }
+        else
+        {
+            Debug.Log("Please set a screen effect canvas in jack's healthbar in the UI");
+        }
     }
 }
